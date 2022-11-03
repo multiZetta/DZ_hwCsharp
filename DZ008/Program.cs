3c64018ef609fd9e7838e6cd2906e8bee1986940
multiZetta <multi@Zetta.ru>
2022-11-03 19:02:25 +0400

    Написан код к задаче 23

---
+++ DZ008/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23
+// Напишите программу, которая принимает 
+// на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите цифру: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Таблица кубов от 1 до " + N + ":  ");
+for (int j = 1; j <= N; j++)
+{
+    Console.WriteLine($"{j} * {j} * {j} = {j * j * j}");
+}
+Console.WriteLine();
